1841bf8767d48858df89bd5dc9ca6c5486c0cb49
14750854-cmyk <14750854@me.mcu.edu.tw>
2026-04-01 11:44:43 +0800

    week06

---
+++ week06/week06-3.c
@@ -0,0 +1,10 @@
+// week06-3.cpp SOIT107_Base_003
+#include <stdio.h>
+int main()
+{
+    printf("Enter two numbers: ");
+    int a, b;
+    scanf("%d%d", &a, &b);
+    //printf("%d", a%b);
+    printf("The remainder is %d\n", a%b);
+}
---
+++ week06/week06-6.c
@@ -0,0 +1,12 @@
+// week06-6.cpp SOIT107_Base_011
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+
+    if (b < a) a = b;
+    if (c < a) a = c;
+
+    printf("%d\n", a);
+}
